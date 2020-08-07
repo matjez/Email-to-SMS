b6e490d9d97942b69a933719d200ba40e513450f
matjez <47278813+matjez@users.noreply.github.com>
2020-08-07 19:14:47 +0200

    Update run.py
    Bug fixing and styling

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ import imaplib
 import email
 import webbrowser
 import os
+import time
 
 from email.header import decode_header
 from twilio.rest import Client
 from tinydb import TinyDB, Query
 
 
-db = TinyDB('db.json')
-
 if os.path.exists('data.txt'):
+    
     data = {}
     f = open("data.txt","r")
     list_of_lines = f.readlines()
@@ -22,62 +22,58 @@ if os.path.exists('data.txt'):
     f.close()
 
 max_number_of_messages = int(data["max_number_of_messages"])
+loops_without_sms = int(data["number_of_emails_to_check"])
 
 
 def send_SMS(number,from_who,message,msg_id):
-    global max_number_of_messages
+    global max_number_of_messages, loops_without_sms 
 
     if data["followed_email"] in from_who:
+
+        db = TinyDB('db.json')
         get_id = Query()
         email_id_in_db = db.search(get_id.id==msg_id)
 
         if len(email_id_in_db) == 0 and max_number_of_messages > 0:
             db.insert({'id': msg_id})
 
-            print(number, from_who, message)
+            if loops_without_sms > 0:
+                loops_without_sms -= 1
 
-            client = Client(data["twillio_sid"], data["twillio_auth_token"])
+            else:
 
+                client = Client(data["twillio_sid"], data["twillio_auth_token"])
+                body = "{} {}".format(from_who,message)
+                length = int(data["max_length_of_sms"])-len(body)
 
-            body = "Mail from {} ".format(from_who)
+                if length < 0:
+                    body = body[:int(data["max_length_of_sms"])]
+                    
+                max_number_of_messages -= 1
+ 
+                print(body, max_number_of_messages, "messages left.")
+                
+                message = client.messages \
+                                .create(
+                                    body=body,
+                                    from_=data["source_phone_number"],
+                                    to=data["phone_number_destintion"]
+                            )
+                            
+                print("SMS sent to {}.".format(data["phone_number_destintion"]))
 
-            length = 70-len(body)
-
-            if length > 0:
-                if len(message) > length:
-                    body += message[:length]
-                else:
-                    body += message     
-
-            print(body)
-
-            max_number_of_messages -= 1
-
-            print(max_number_of_messages, "messages left.")
-
-            # zrobic sprawdzanie dlugosci smsa i dalej loopa 
-
-            # message = client.messages \
-            #                 .create(
-            #                     body=message,
-            #                     from_=data["SMS_header"],
-            #                     to=data["phone_number_destintion"]
-            #                 )
-
-            # print(message.sid)
+        else:
+            loops_without_sms -= 1 
 
     else:
         return 
 
-
 def update_status():
 
     imap = imaplib.IMAP4_SSL("imap.gmail.com")
-
     imap.login(data["email"], data["password"])
 
     status, messages = imap.select("INBOX")
-
     messages = int(messages[0])
 
     for i in range(messages, messages-int(data["number_of_emails_to_check"]), -1):
@@ -88,10 +84,9 @@ def update_status():
             if isinstance(response, tuple):
                 
                 msg = email.message_from_bytes(response[1])
-                
                 subject = decode_header(msg["Subject"])[0][0]
-                if isinstance(subject, bytes):
 
+                if isinstance(subject, bytes):
                     subject = subject.decode("latin2")
 
                 msg_id = msg.get("Message-ID")
@@ -107,24 +102,24 @@ def update_status():
                             body = part.get_payload(decode=True).decode("latin2")
                         except:
                             pass
-                            
+                        
                         if content_type == "text/plain" and "attachment" not in content_disposition:
                             send_SMS(783621421,from_,body,msg_id)
 
                 else:
                     content_type = msg.get_content_type()
                     body = msg.get_payload(decode=True).decode("latin2")
+
                     if content_type == "text/plain":
                         send_SMS(783621421,str(from_),body,msg_id)
 
     imap.close()
     imap.logout()            
 
-print("Running...")
+
+print("Running...\n")
 
 while True:
-    x = input("Type y to refresh q to quit: ")
-    if x == "y" or x == "Y":
-        update_status()
-    else: 
-        break
+    time.sleep(int(data["interval"]))
+    update_status()
+
